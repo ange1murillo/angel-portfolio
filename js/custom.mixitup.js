c969963ee170f2547c296e5f825a0ebb87994177
ange1murillo <angelmurill8@gmail.com>
2019-09-12 02:09:29 -0500

    fixed project bug

--- js/custom.mixitup.js
+++ js/custom.mixitup.js
@@ -1,4 +1,4 @@
-var mixer = mixitup('.projects');
+// var mixer = mixitup('.projects');
 
 // var mixer = mixitup(containerEl);
 
@@ -9,4 +9,20 @@ var mixer = mixitup('.projects');
 // 	animation: {
 // 		duration: 100
 // 	}
+// });
+
+var mixer = mixitup('.projects', {
+	selectors: {
+		control: '[data-mixitup-control]',
+	},
+});
+
+// var mixer = mixitup({
+// 	...$,
+// 	templates: {
+// 		pager:
+// 			'<button type="button" class="${classNames}" data-page="${pageNumber}" data-mixitup-control>${pageNumber}</button>',
+// 		pagerPrev: '<button type="button" class="${classNames}" data-page="prev" data-mixitup-control>&laquo;</button>',
+// 		pagerNext: '<button type="button" class="${classNames}" data-page="next" data-mixitup-control>&raquo;</button>',
+// 	},
 // });
